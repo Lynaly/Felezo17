fbeaf49c42f97bfda4bae57c1c4834ea60821b3e
Adam Torok <adamtorok@sch.bme.hu>
2017-02-07 16:50:47 +0100

    Orders controller and view added

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\Http\Controllers;
+
+
+class OrdersController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ Route::group(['prefix' => 'news', 'as' => 'news.'], function ()
 Route::get('programs', 'ProgramsController@index')->name('programs.index');
 
 Route::get('location', 'LocationController@index')->name('location.index');
+
+Route::group(['prefix' => 'orders', 'as' => 'orders.'], function ()
+{
+    Route::get('/', 'OrdersController@index')->name('index');
+});
