a362a8645ed0fff1a7c5c75c79fc20483b198490
Adam Torok <adamtorok@sch.bme.hu>
2017-03-09 14:22:39 +0100

    Login redirect back to orders

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -77,7 +77,7 @@ class AuthController extends Controller
             Auth::login($user);
         }
 
-        return redirect()->intended('/');
+        return redirect()->intended('/orders');
     }
 
     private function getUnit($units)
