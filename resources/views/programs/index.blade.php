88a673a39440cbd4dba6e7350f81d61ac5065aa1
Adam Torok <adamtorok@sch.bme.hu>
2017-03-30 14:18:26 +0200

    programs added

--- resources/views/programs/index.blade.php
+++ resources/views/programs/index.blade.php
@@ -2,5 +2,7 @@
 @section('title', 'Programok')
 @section('icon', 'calendar')
 @section('content')
-   <h3>Hamarosan...</h3>
+   <h3><b>18:00</b> Kapunyitás</h3>
+   <h3><b>18:30</b> Megnyitó és a vacsora kezdete</h3>
+   <h3><b>22:30 - 4:00</b> After party</h3>
 @endsection
